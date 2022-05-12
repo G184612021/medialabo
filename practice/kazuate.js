162b3e1f455812124fea6bb03212f5e6b7e9af85
G184612021 <218461@st.takushoku-u.ac.jp>
2022-05-12 14:48:52 +0900

    kadai

--- practice/kazuate.js
+++ practice/kazuate.js
@@ -16,7 +16,26 @@ hantei();
 function hantei() {
     let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
     // 課題3-1：ここの判定処理を作成する．
-    //        ページに表示する方法はまだ習っていないので
-    //        判定結果はコンソールに出力すること
+    kaisu = kaisu + 1;
+    console.log(kaisu +"回目の予想");
+    if(kaisu >= 4 ){
+        console.log("答えは" + kotae + "でした。すでにゲームは終わっています");
+    }else if (3 > kaisu ){
+        if (kotae === yoso){
+            
+            console.log("正解です。おめでとう！");
+        }else if (kotae > yoso){
+            console.log("まちがい。答えはもっと小さいですよ");
 
+        }else if (kotae < yoso){
+            console.log("まちがい。答えはもっと大きいですよ")
+        }
+        //        ページに表示する方法はまだ習っていないので
+        //        判定結果はコンソールに出力すること
+    }else if (kaisu === 3){
+        if (kotae === yoso){
+            console.log("正解です。おめでとう！");
+        }else 
+        console.log("まちがい。残念でした答えは" + kotae + "です。");
+    }
 }
